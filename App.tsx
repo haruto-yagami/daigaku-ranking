f944c1a80c597a6713b95bde5fb294d3c8773bd5
haruto-yagami <83498249+haruto-yagami@users.noreply.github.com>
2025-12-09 15:03:34 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
+import  Header  from './components/Header';
 import { Home } from './pages/Home';
 import { ChevronUp } from 'lucide-react';
 
